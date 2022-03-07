c3f6480e56990a2c9eabd0d0b4694339ac21f041
Owen Dresner <owendrez@gmail.com>
2022-03-07 17:14:07 -0500

    commit tree 31a762c26e7c24f39b247bf603a46878b4e0fb62

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return "This Saturday, I want to "+activity+"!" 
+}
+function mondayWork(activity="go to the office"){
+    return "This Monday, I will "+activity+"."
+}
+function wrapAdjective(value="*"){
+    return function(value2="special"){
+        return "You are "+value+value2+value+"!"
+
+    }
+}
